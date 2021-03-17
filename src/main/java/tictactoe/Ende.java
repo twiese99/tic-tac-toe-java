b7df7d92a3b789fee9594506a57248afca72fda6
JannisT <jannis@xrow.de>
2021-03-17 14:20:24 +0100

    Add basic class files for further handling

---
+++ src/main/java/tictactoe/Ende.java
@@ -0,0 +1,18 @@
+package tictactoe;
+
+public class Ende implements Aktion {
+
+	private Context context;
+
+	public Ende(Context c) {
+		
+	}
+	
+	public void keyPressed(char c) {
+		
+	}
+	
+	public void numPressed(int i) {
+		
+	}
+}
---
+++ src/main/java/tictactoe/World.java
@@ -0,0 +1,38 @@
+package tictactoe;
+
+public class World {
+	
+	private char[][] board;
+
+	
+	public char[][] getBoard() {
+		return board;
+	}
+	
+	public void setStone(char c, int x, int y) {
+		
+	}
+	
+	public boolean isWon() {
+		
+		//return true;
+		
+		//return false;
+	}
+	
+	public char getStone(int x, int y) {
+		// ???
+		
+	}
+	
+	public boolean isDrawn() {
+		
+		//return true;
+		
+		//return false;
+	}
+	
+	public String toString() {
+		
+	}
+}
