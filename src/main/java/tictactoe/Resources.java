9a776c1604bfebc40376569383d6f992d0dd8c04
Tails32 <tails32@hotmail.de>
2021-03-19 11:30:04 +0100

    Resources: added AnsiArt.BOARD

--- src/main/java/tictactoe/Resources.java
+++ src/main/java/tictactoe/Resources.java
@@ -40,6 +40,15 @@ public final class Resources {
                                                      [ PRESS ANY KEY ]                                     
                                                                                                            
                 """;
+        public static final String BOARD = """
+                ┌─────┬─────┬─────┐
+                │ {0} │ {1} │ {2} │
+                ├─────┼─────┼─────┤
+                │ {3} │ {4} │ {5} │
+                ├─────┼─────┼─────┤
+                │ {6} │ {7} │ {8} │
+                └─────┴─────┴─────┘
+                """;
     }
 
     public static final class PlayerSymbol {
